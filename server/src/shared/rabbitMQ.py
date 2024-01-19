9917a2d2c333bd57d7a31410c06598469dc4b050
fede4ka1245 <bragin.fedor96@gmail.com>
2024-01-19 11:58:40 +0500

    commit changes

--- server/src/shared/rabbitMQ.py
+++ server/src/shared/rabbitMQ.py
@@ -1,7 +1,7 @@
 import pika
-from .config import RABBIT_MQ_HOST
+from .config import RABBIT_MQ_HOST, RABBIT_MQ_PASS, RABBIT_MQ_LOGIN
 
 
 def connect_rabbitmq():
-    credentials = pika.PlainCredentials('rmuser', 'rmpassword')
+    credentials = pika.PlainCredentials(RABBIT_MQ_LOGIN, RABBIT_MQ_PASS)
     return pika.BlockingConnection(pika.ConnectionParameters(RABBIT_MQ_HOST, 5672, credentials=credentials))
