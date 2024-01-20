8dc8bfbc964f3e7fd958a6e7ebd0c6870988d853
fede4ka1245 <bragin.fedor96@gmail.com>
2024-01-20 15:27:20 +0500

    fix web app

---
+++ frontend/src/ui/drawer/Drawer.js
@@ -0,0 +1,14 @@
+import { styled } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+
+export default styled(MuiDrawer)(({ theme }) => ({
+  '& .MuiPaper-root': {
+    height: 'calc(100vh - 80px)',
+    width: '100vw',
+    background: 'var(--bg-color)',
+    overflow: 'visible',
+    borderRadius: 'var(--border-radius-lg) var(--border-radius-lg) 0 0',
+    margin: 'auto 0',
+    maxWidth: 'var(--max-content)',
+  },
+}));
